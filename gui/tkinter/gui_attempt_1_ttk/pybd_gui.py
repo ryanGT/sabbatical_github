7a724d87e32689c5e44745ecb2194c9814b39725
Ryan Krauss <ryanwkrauss@gmail.com>
2022-02-15 14:46:31 -0500

    working on add block dialog

--- gui/tkinter/gui_attempt_1_ttk/pybd_gui.py
+++ gui/tkinter/gui_attempt_1_ttk/pybd_gui.py
@@ -30,6 +30,58 @@ from tkinter.messagebox import showinfo
 # Create style for the first frame
 #s.configure('Frame1.TFrame', background='red')
 
+from tkinter import simpledialog
+
+class MyDialog(simpledialog.Dialog):
+    def __init__(self, parent, title):
+        self.my_username = None
+        self.my_password = None
+        super().__init__(parent, title)
+
+
+    def body(self, frame):
+        # print(type(frame)) # tkinter.Frame
+        self.my_username_label = tk.Label(frame, width=25, text="Username")
+        self.my_username_label.pack()
+        self.my_username_box = tk.Entry(frame, width=25)
+        self.my_username_box.pack()
+
+        self.my_password_label = tk.Label(frame, width=25, text="Password")
+        self.my_password_label.pack()
+        self.my_password_box = tk.Entry(frame, width=25)
+        self.my_password_box.pack()
+        self.my_password_box['show'] = '*'
+
+        return frame
+
+
+    def ok_pressed(self):
+        # print("ok")
+        self.my_username = self.my_username_box.get()
+        self.my_password = self.my_password_box.get()
+        self.destroy()
+
+
+    def cancel_pressed(self):
+        # print("cancel")
+        self.destroy()
+
+
+    def buttonbox(self):
+        self.ok_button = tk.Button(self, text='OK', width=5, command=self.ok_pressed)
+        self.ok_button.pack(side="left")
+        cancel_button = tk.Button(self, text='Cancel', width=5, command=self.cancel_pressed)
+        cancel_button.pack(side="right")
+        self.bind("<Return>", lambda event: self.ok_pressed())
+        self.bind("<Escape>", lambda event: self.cancel_pressed())
+
+
+#def mydialog(app):
+#    dialog = MyDialog(title="Login", parent=app)
+#    return dialog.my_username, dialog.my_password
+
+
+
 class pybd_gui(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -51,9 +103,12 @@ class pybd_gui(tk.Tk):
 
 
     def add_block(self):
-        showinfo(title='Information',
-                 message='add block pressed')
+        #showinfo(title='Information',
+        #        message='add block pressed')
+        mydialog = MyDialog(title="Add New Block", parent=self)
+        print("%s, %s" % (mydialog.my_username, mydialog.my_password))
 
+        
     def _quit(self):
         self.quit()     # stops mainloop
         self.destroy()  # this is necessary on Windows to prevent
