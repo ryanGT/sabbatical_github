f1692169b21a2614a31e0075a52a4cfb18295a78
Ryan Krauss <ryanwkrauss@gmail.com>
2022-02-16 13:01:36 -0500

    random backup

--- gui/tkinter/gui_attempt_1_ttk/pybd_gui.py
+++ gui/tkinter/gui_attempt_1_ttk/pybd_gui.py
@@ -5,6 +5,32 @@ Embedding in Tk
 
 """
 
+
+#iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+#
+# Issues:
+#
+# - how do I handle sensors and actuators so that plants can be
+#   created?
+#     - gui could show actuator and sensor comboboxes when plant is selected
+#         - or only sensor for plant_no_actuator
+#
+# - how do I handle cases with input(s) set when creating new blocks?
+#     - pass kwargs to the create block function?
+#         - I need to handle blocks with more than one input differently
+#             - input vs. input1
+#     - how do I make this work without marrying it to the gui?
+#         - but only the gui knows if an input was chosen
+#         - the gui can pass along chosen input name(s)
+#         - the create block function in pydb can handle
+#           input vs. input1 depending on block type
+#
+# Resovled:
+# - selecting a block type and then selecting an input block messes up
+#   the current selection of the block type listbox
+#  
+#iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+
 import tkinter
 import tkinter as tk
 from tkinter import ttk
@@ -32,17 +58,23 @@ from tkinter.messagebox import showinfo
 
 from tkinter import simpledialog
 
-import py_block_diagram
+import py_block_diagram as pybd
+
+pad_options = {'padx': 5, 'pady': 5}
 
 #class MyDialog(simpledialog.Dialog):
 class MyDialog(tk.Toplevel):
     def __init__(self, parent, title="Add Block Dialog"):
         super().__init__(parent)
-
-        self.geometry('500x400')
+        self.selected_block_type = None
+        self.input_block_name = None
+        self.input2_block_name = None
+        self.input3_block_name = None
+        self.parent = parent
+        self.geometry('800x600')
         self.title(title)
         self.make_widgets()
-        self.parent = parent
+
         
 
     #def __init__(self, parent, title):
@@ -57,7 +89,13 @@ class MyDialog(tk.Toplevel):
         #def body(self):
         #print("frame: %s" % frame)
         # print(type(frame)) # tkinter.Frame
-        self.label1 = ttk.Label(self, width=25, text="Block Category")
+
+        #=================================
+        #
+        # column 0 
+        #
+        #=================================
+        self.label1 = ttk.Label(self, text="Block Category")
 
 
         #self.label1.grid(row=0, column=0)
@@ -66,45 +104,120 @@ class MyDialog(tk.Toplevel):
         self.category_combobox = ttk.Combobox(self, textvariable=self.selected_category)
         
         # get first 3 letters of every month name
-        self.category_combobox['values'] = py_block_diagram.block_categories
+        self.category_combobox['values'] = pybd.block_categories
         
         # prevent typing a value
         self.category_combobox['state'] = 'readonly'
+        self.category_combobox.bind('<<ComboboxSelected>>', self.category_selected)
+        start_key = 'input'
+        self.category_combobox.set(start_key)
+        label2 = ttk.Label(self, text="Block Type")
 
-        label2 = ttk.Label(self, width=25, text="Block Type")
-
-        self.selected_block = tk.StringVar(value=[])
+        self.block_choice_list = tk.StringVar(value=pybd.block_category_dict[start_key])
 
         self.blockchoice = tk.Listbox(self, \
-                                      listvariable=self.selected_block, \
+                                      listvariable=self.block_choice_list, \
                                       height=6, \
+                                      width=40, \
                                       #selectmode='extended'
                                       )
 
-
+        self.blockchoice.bind("<<ListboxSelect>>", self.on_block_type_selected)
         #self.category_combobox.grid(row=1, column=0)
 
         column0_widgets = [self.label1, self.category_combobox, label2, self.blockchoice]
 
         for i, widget in enumerate(column0_widgets):
-            widget.grid(row=i, column=0)
+            widget.grid(row=i, column=0, sticky='W', **pad_options)
             
 
-        self.my_password_label = ttk.Label(self, width=25, text="Password")
-        self.my_password_label.grid(row=0, column=1)
-        self.my_password_box = ttk.Entry(self, width=25)
-        self.my_password_box.grid(row=1, column=1,sticky="")
-        self.my_password_box['show'] = '*'
+        #=================================
+        #
+        # column 1 
+        #
+        #=================================
+        label_BN = ttk.Label(self, text="Block Name")
 
-        self.ok_button = ttk.Button(self, text='OK', width=5, command=self.ok_pressed)
-        self.ok_button.grid(row=2, column=1)
+        self.block_name = tk.StringVar()
+        self.block_name_box = ttk.Entry(self, textvariable=self.block_name)
+
+        label_input = ttk.Label(self, text="Input Block")
+        self.input_block_list = tk.StringVar(value=self.parent.get_block_name_list())
+        
+        self.input_choice = tk.Listbox(self, \
+                                       listvariable=self.input_block_list, \
+                                       height=6, \
+                                       width=25, \
+                                       #selectmode='extended'
+                                       )
+        self.input_choice.bind("<<ListboxSelect>>", self.on_input_selected)
+        
+        self.go_button = ttk.Button(self, text='Add Block', command=self.go_pressed)
+
+        column1_widgets = [label_BN, self.block_name_box, label_input, \
+                           self.input_choice, self.go_button]
+        for i, widget in enumerate(column1_widgets):
+            widget.grid(row=i, column=1, sticky='W', **pad_options)
 
 
-    def ok_pressed(self):
-        # print("ok")
-        #self.my_username = self.my_username_box.get()
-        self.my_password = self.my_password_box.get()
-        self.parent.password = self.my_password
+    def on_input_selected(self, *args):
+        selection = self.input_choice.curselection()
+        print("input selection:")
+        print(selection)
+        if selection:
+            self.input_block_name = self.get_input_block_name()
+            print("input selected: %s" % self.input_block_name)
+        else:
+            print("no selection")
+        
+        
+    def on_block_type_selected(self, *args):
+        selection = self.blockchoice.curselection()
+        print("block type selection:")
+        print(selection)
+        if not selection:
+            print("no selection")
+            return None
+        block_type = self.get_selected_block_type()
+        print("block_type: %s" % block_type)
+        self.selected_block_type = block_type
+        suggested_name = self.parent.block_diagram.suggest_block_name(block_type)
+        self.block_name.set(suggested_name)
+        
+
+    def get_selected_block_type(self):
+        block_type = self.blockchoice.get(self.blockchoice.curselection())
+        return block_type
+
+
+    def get_input_block_name(self):
+        input_name = self.input_choice.get(self.input_choice.curselection())
+        return input_name
+    
+    
+    def go_pressed(self):
+        assert self.selected_block_type is not None, "block_type has not been set"
+        print("you pressed go")
+        block_type = self.selected_block_type
+        print("block_type: %s" % block_type)
+        kwargs = {}
+
+        # ultimately, input_block_names need to be converted to actual block instances
+        # - look up the block in parent.block_diagram
+        if self.input_block_name is not None:
+            print("input block name: %s" % self.input_block_name)
+            kwargs['input'] = self.input_block_name
+        if self.input2_block_name is not None:
+            kwargs['input2'] = self.input2_block_name
+        if self.input3_block_name is not None:
+            kwargs['input3'] = self.input3_block_name
+            
+        block_name = self.block_name.get()
+        block_class = getattr(pybd, block_type)
+        # how do I handle cases with input(s) set?
+        new_block = pybd.create_block(block_class, block_type, block_name, **kwargs)
+        self.parent.append_block_to_dict(block_name, new_block)
+        #self.parent.password = self.my_password
         self.destroy()
 
 
@@ -113,6 +226,20 @@ class MyDialog(tk.Toplevel):
         self.destroy()
 
 
+    def category_selected(self, event):
+        chosen_cat = self.selected_category.get()
+        print("category_selected: %s" % chosen_cat)
+        new_list = pybd.block_category_dict[chosen_cat]
+        print("new_list: %s" % new_list)
+        #lb.delete(0,END)
+        #Label(win, text="Nothing Found Here!",
+        #font=('TkheadingFont, 20')).pack()
+
+        # Add items in the Listbox
+        #lb.insert("end","item1","item2","item3","item4","item5")
+
+        self.block_choice_list.set(new_list)
+        
 #    def buttonbox(self):
 #        self.ok_button = tk.Button(self, text='OK', width=5, command=self.ok_pressed)
 #        self.ok_button.pack(side="left")
@@ -147,7 +274,21 @@ class pybd_gui(tk.Tk):
         # configure the root window
         self.make_widgets()
 
+        self.block_diagram = pybd.block_diagram()
+        
 
+    def get_block_name_list(self):
+        #block_list = self.block_diagram._build_block_list()
+        mylist = self.block_diagram.block_name_list
+        return mylist
+
+
+    def append_block_to_dict(self, block_name, new_block):
+        self.block_diagram.append_block_to_dict(block_name, new_block)
+        # update listbox
+        self.block_list_var.set(self.block_diagram.block_name_list)
+        
+        
     def add_block(self):
         #showinfo(title='Information',
         #        message='add block pressed')
@@ -218,10 +359,7 @@ class pybd_gui(tk.Tk):
         self.block_label = ttk.Label(self, text="Blocks")
         self.block_label.grid(row=0,column=cur_col,sticky='NW', **self.options)
 
-        langs = ('Java', 'C#', 'C', 'C++', 'Python',
-                 'Go', 'JavaScript', 'PHP', 'Swift')
-
-        self.block_list_var = tk.StringVar(value=langs)
+        self.block_list_var = tk.StringVar(value=[])
 
         self.blocklistbox = tk.Listbox(self, \
                                         listvariable=self.block_list_var, \
