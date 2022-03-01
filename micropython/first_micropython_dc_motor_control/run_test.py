84003c21858e5cdafe62352912086d44e01f163b
Ryan Krauss <ryanwkrauss@gmail.com>
2022-03-01 18:44:37 -0500

    P control with data

--- micropython/first_micropython_dc_motor_control/run_test.py
+++ micropython/first_micropython_dc_motor_control/run_test.py
@@ -2,7 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pyboard
 portname = "/dev/tty.usbmodem207831A44E532"
-output = pyboard.execfile("main.py", device=portname)
+#fn = "main_PD_control.py"
+#fn = "main.py"
+fn = "main_P_control.py"
+output = pyboard.execfile(fn, device=portname)
 mystr = output.decode()
 mylist = mystr.split('\r\n')
 mylist.pop(-1)
@@ -11,13 +14,21 @@ mylist.pop(0)
 mylist_delim = [row.split(',') for row in mylist]
 mystrarray = np.array(mylist_delim)
 data = mystrarray.astype(int)
-enc = data[:,0]*256+data[:,1]
+#data[i,0] = enc
+#data[i,1] = e
+#data[i,2] = v_i
+enc = data[:,0]
+e = data[:,1]
+v = data[:,2]
+
 N = len(enc)
 u = np.zeros(N, dtype=np.int16)
-u[10:100] = 200
+u[10:] = 200
 
 plt.figure(1)
 plt.plot(u)
 plt.plot(enc)
+plt.plot(e)
+plt.plot(v)
 
 plt.show()
